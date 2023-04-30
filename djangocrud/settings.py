2515127c4afcf205309fa935e17115e3fc0e1c9c
poldiaz50 <108167418+poldiaz50@users.noreply.github.com>
2023-04-30 04:34:36 -0500

    modifica settings.py

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'djangocrud.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
+    default: {
         #'ENGINE': 'django.db.backends.sqlite3',
         #'NAME': BASE_DIR / 'db.sqlite3',
         dj_database_url.config(
